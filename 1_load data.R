31cfe010a8022e36610f0ed1df9df3c031b8f264
Hee Won Kim <58554475+Heewon-Hailey@users.noreply.github.com>
2022-01-04 17:01:34 +0900

    Update and rename load data.R to 1_load data.R

--- 1_load data.R
+++ 1_load data.R
@@ -1,5 +1,5 @@
-
-my.df2<-read.csv('/Users/heekim/Ass1-ori_results.csv', 
+# load data as df2
+my.df2<-read.csv('/results.csv',     # path of result file
                       header = T,    # set columns names true
                       sep = ",",     # define the separator between columns
                       skip = 6,      # skip first 6 rows
@@ -7,18 +7,12 @@ my.df2<-read.csv('/Users/heekim/Ass1-ori_results.csv',
                       fill = TRUE
 )
 
-# view (myrm(.df)
+# view in boxplot
 boxplot(total_infected~average_immune_time,
         data = my.df.new ,
         col = "grey",
         main = "Total number of infected cases - Initial Vaccination",
         xlab = "Initial Vaccination (%)",
         ylab = "Total number of infected (cases)"
-        
-        #        total_infected~average_immune_time,
-        #        data = my.df.new ,
-        #        col = "grey",
-        #        main = "Total number of infected cases - Initial Vaccination",
-        #        xlab = "Initial Vaccination (%)",
-        #        ylab = "Total number of infected (cases)"
+
 )
